195e3c0611b313679417075be611d6af9f59be80
Saori Kohno <SaoKKK@users.noreply.github.com>
2015-02-27 22:14:58 +0900

    Initial Commit

---
+++ ClearIcon/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ClearIcon
+//
+//  Created by 河野 さおり on 2015/02/27.
+//  Copyright (c) 2015年 Saori Kohno. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
